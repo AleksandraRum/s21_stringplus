854d692035331d81b0bd2829dc7d9f3630845ac4
Revachol <egorov.dima1470@gmail.com>
2023-11-22 21:28:28 +0300

    add base

---
+++ src/s21_string.c
@@ -0,0 +1,5 @@
+#include "s21_string.h"
+
+int main(){
+    return 0;
+}
---
+++ src/s21_string.h
@@ -0,0 +1,16 @@
+#ifndef s21_string
+#define s21_string
+
+#include <dirent.h>
+#include <getopt.h>
+#include <regex.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFFER 10000
+#define s21_size_t unsigned long long
+#define s21_NULL (void*)0
+
+#endif
