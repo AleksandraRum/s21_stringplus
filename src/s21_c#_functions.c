c6899ca80a17afd935d8433dbc1a56a92c338759
Revachol <egorov.dima1470@gmail.com>
2023-12-12 08:50:18 +0300

    complete to_upper to_lower

---
+++ src/s21_c#_functions.c
@@ -0,0 +1,39 @@
+#include "s21_c#_functions.h"
+
+void* to_lower(const char * str){
+    char *str_copy = s21_NULL;
+    if(str != s21_NULL){
+        s21_size_t len = s21_strlen(str);
+        str_copy = (char *)malloc(sizeof(char) * (len + 1));
+        if(str_copy){
+            for(s21_size_t i = 0; i <= len; i++){
+                if(str[i] >= 'A' && str[i] <= 'Z'){
+                    str_copy[i] = (str[i] - 'A') + 'a';
+                }else{
+                    str_copy[i] = str[i];
+                }
+            }
+            str_copy[len] = '\0';
+        }
+    }
+    return str_copy;
+}
+
+void* to_upper(const char * str){
+    char *str_copy = s21_NULL;
+    if(str != s21_NULL){
+        s21_size_t len = s21_strlen(str);
+        str_copy = (char *)malloc(sizeof(char) * (len + 1));
+        if(str_copy){
+            for(s21_size_t i = 0; i <= len; i++){
+                if(str[i] >= 'a' && str[i] <= 'z'){
+                    str_copy[i] = (str[i] - 'a') + 'A';
+                }else{
+                    str_copy[i] = str[i];
+                }
+            }
+            str_copy[len] = '\0';
+        }
+    }
+    return str_copy;
+}
