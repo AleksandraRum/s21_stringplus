2ba68f9ace8cc8db087e60c412cd7f6ab4b1c51f
Alex Valukh <valukh56@gmail.com>
2023-12-17 01:39:27 +0300

    error fix_2

--- src/s21_strerror.c
+++ src/s21_strerror.c
@@ -1,13 +1,15 @@
 #include "s21_string.h"
 
 char *s21_strerror(int errnum) {
-    char *s_err;
+    char* s_err;
     char* ERRORS[] = ERR_LIST;
     if (errnum >= 0 && errnum <= 133){
         s_err = (char*)ERRORS[errnum];
     }
     else {
-        s_err = "Unknown error";
+        static char buffer[80];
+        s21_sprintf(buffer, "Unknown error %d", errnum);
+        s_err = buffer;
     }
     return s_err;
 }
