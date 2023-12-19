6c6cd67542e8bbf1650abc4208ef7aee9f5a8873
Hardy Deloris <hardydel@ox-p4.msk.21-school.ru>
2023-12-19 17:37:53 +0300

    complete sharp functions

--- src/s21_c#_functions.c
+++ src/s21_c#_functions.c
@@ -1,39 +1,110 @@
 #include "s21_c#_functions.h"
 
-void* to_lower(const char * str){
-    char *str_copy = s21_NULL;
-    if(str != s21_NULL){
-        s21_size_t len = s21_strlen(str);
-        str_copy = (char *)malloc(sizeof(char) * (len + 1));
-        if(str_copy){
-            for(s21_size_t i = 0; i <= len; i++){
-                if(str[i] >= 'A' && str[i] <= 'Z'){
-                    str_copy[i] = (str[i] - 'A') + 'a';
-                }else{
-                    str_copy[i] = str[i];
-                }
-            }
-            str_copy[len] = '\0';
+void *s21_to_lower(const char *str) {
+  char *str_copy = s21_NULL;
+  if (str != s21_NULL) {
+    s21_size_t len = s21_strlen(str);
+    str_copy = (char *)malloc(sizeof(char) * (len + 1));
+    if (str_copy) {
+      for (s21_size_t i = 0; i <= len; i++) {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+          str_copy[i] = (str[i] - 'A') + 'a';
+        } else {
+          str_copy[i] = str[i];
         }
+      }
+      str_copy[len] = '\0';
     }
-    return str_copy;
+  }
+  return str_copy;
 }
 
-void* to_upper(const char * str){
-    char *str_copy = s21_NULL;
-    if(str != s21_NULL){
-        s21_size_t len = s21_strlen(str);
-        str_copy = (char *)malloc(sizeof(char) * (len + 1));
-        if(str_copy){
-            for(s21_size_t i = 0; i <= len; i++){
-                if(str[i] >= 'a' && str[i] <= 'z'){
-                    str_copy[i] = (str[i] - 'a') + 'A';
-                }else{
-                    str_copy[i] = str[i];
-                }
-            }
-            str_copy[len] = '\0';
+void *s21_to_upper(const char *str) {
+  char *str_copy = s21_NULL;
+  if (str != s21_NULL) {
+    s21_size_t len = s21_strlen(str);
+    str_copy = (char *)malloc(sizeof(char) * (len + 1));
+    if (str_copy) {
+      for (s21_size_t i = 0; i <= len; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+          str_copy[i] = (str[i] - 'a') + 'A';
+        } else {
+          str_copy[i] = str[i];
         }
+      }
+      str_copy[len] = '\0';
     }
-    return str_copy;
+  }
+  return str_copy;
+}
+
+void *s21_insert(const char *src, const char *str, s21_size_t start_index) {
+  char *res = s21_NULL;
+  if (src != s21_NULL && str != s21_NULL) {
+    s21_size_t src_len = s21_strlen(src);
+    s21_size_t str_len = s21_strlen(str);
+    s21_size_t final_len = src_len + str_len;
+    if (str_len >= start_index) {
+      res = (char *)malloc(sizeof(char) * (final_len + 1));
+    }
+    if (res) {
+      s21_size_t j = 0, k = 0;
+      for (s21_size_t i = 0; i < final_len; i++) {
+        if (i >= start_index && j < str_len) {
+          res[i] = str[j];
+          j++;
+        } else {
+          res[i] = src[k];
+          k++;
+        }
+      }
+      res[final_len] = '\0';
+    }
+  }
+  return res;
+}
+
+void *s21_trim(const char *src, const char *trim_chars) {
+  char *res = s21_NULL;
+  if (src != s21_NULL) {
+    if (trim_chars != s21_NULL && *trim_chars) {
+      s21_size_t src_len = strlen(src);
+      s21_size_t trim_len = strlen(trim_chars);
+      res = (char *)malloc(sizeof(char) * (src_len + 1));
+      if (res) {
+        s21_size_t left = 0, right = 0;
+        for (s21_size_t i = 0; i < src_len; i++) {
+          int flag = 1;
+          for (s21_size_t j = 0; j < trim_len; j++) {
+            if (src[i] == trim_chars[j]) {
+              flag = 0;
+            }
+          }
+          if (flag) {
+            left = i;
+            i = src_len;
+          }
+        }
+        for (int i = src_len - 1; i >= 0; i--) {
+          int flag = 1;
+          for (s21_size_t j = 0; j < trim_len; j++) {
+            if (src[i] == trim_chars[j]) {
+              flag = 0;
+            }
+          }
+          if (flag) {
+            right = i;
+            i = -1;
+          }
+        }
+        for (s21_size_t i = 0; i < right - 1; i++) {
+          res[i] = src[i + left];
+        }
+        res[src_len - right - 1] = '\0';
+      }
+    } else {
+      res = s21_trim(src, " \t\n");
+    }
+  }
+  return res;
 }
--- src/s21_c#_functions.h
+++ src/s21_c#_functions.h
@@ -1,8 +1,11 @@
 #ifndef S21_C_SHARP_FUNC
 #define S21_C_SHARP_FUNC
-    #include "s21_string.h"
-    #include <stdlib.h>
+#include <stdlib.h>
 
-    void* to_lower(const char * str);
-    void* to_upper(const char * str);
+#include "s21_string.h"
+
+void* s21_to_lower(const char* str);
+void* s21_to_upper(const char* str);
+void* s21_insert(const char* src, const char* str, s21_size_t start_index);
+void* s21_trim(const char* src, const char* trim_chars);
 #endif
