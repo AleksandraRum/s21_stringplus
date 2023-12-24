c72ff66caaa82cee006a6335a0564a06e81ecb55
Helga Annis <helgaann@at-q6.msk.21-school.ru>
2023-12-24 18:37:58 +0300

    discad lib string

--- src/s21_string.h
+++ src/s21_string.h
@@ -1,11 +1,8 @@
 #ifndef s21_string
 #define s21_string
 
-//#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <math.h>
 
 #define BUFFER 10000
